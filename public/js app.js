727acaf7d81838bb32020dfebe3a9eb26893435a
SyedHurairaRaza <syedhurairaraza.114@gmail.com>
2022-10-31 02:44:54 +0500

    work complete

---
+++ public/js app.js
@@ -0,0 +1,81 @@
+// alert("sucker ")
+// window.alert("hello");
+
+// var abc ="hello world";
+// alert(abc);
+
+// var b = 10;
+// alert(b);
+
+// var a =10;
+// var b=20;
+// var c= a+b;
+// alert(c);
+
+// alert("thanks for your input!");
+// var name ="raza";
+
+// alert(name);
+
+// var floogle = "mushi";
+// var name = "raza";
+
+// alert(floogle)
+
+// var thanx = "hehehehe";
+// alert(thanx);
+
+// var weight =15;
+// weight+25
+// console.log(weight)
+
+// var originalnum =12;
+// var newnum=originalnum+7;
+// console.log(newnum) 
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum)
+
+// var caseQty = 144;
+// alert(caseQty);
+
+// var num = 2 + 2;
+// alert(num)
+
+// var a= 8+10;
+// var b=3*5;
+// var c=10/2;
+// alert(c) 
+
+// var num = 1;
+// var newNum = ++num;
+// console.log(newNum)
+
+// var num= 5;
+// var newnum=num--;
+// console.log(newnum) 
+
+// var num= 5;
+// var newnum=--num;
+// console.log(newnum)
+
+// var totalCost = 1 + (3 * 4);
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation)
+
+// var resultOfComputation = ((5 * 4) * 9) + 2;
+// console.log(resultOfComputation)
+
+
+// var message = "Thanks, ";
+// var name ="raza";
+// var banger = "!";
+// alert(message + name+ banger);
+
+var message = "Thanks, ";
+var userName = "raza";
+ var banger = "!";
+ var customMess = message + userName + banger;
+ alert(customMess);
